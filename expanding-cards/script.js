6287e911835c57a30ffc23afc85f403834087ae2
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-07-29 14:13:29 -0500

    Using the BEM methodology

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,5 +1,5 @@
 // Funciones
-const click = document.getElementClass('container__panel-active');
+const click = document.querySelectorAll('.container__panel, .container__panel-active');
 
 if (click === Onclick) {
     style
